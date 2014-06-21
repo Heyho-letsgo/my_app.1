155b69dee941c29bd73d20cf48f69b4b0b073a55
Prelang Builder <builder@prelang.com>
2014-06-21 16:42:01 +0000

    Uncommented the root route in 'routes.rb' and pointed it to Acceuilroutes#index

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ MyApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'acceuilroutes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
